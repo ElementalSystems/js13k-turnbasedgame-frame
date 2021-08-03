7c2b6ea368048eff3ca3c3697889a278de6d64cd
AHS <ahigsonsmith@gmail.com>
2021-08-03 22:09:36 +0200

    Basic bits in play

--- public/client.js
+++ public/client.js
@@ -2,119 +2,66 @@
 
 (function () {
 
-    let socket, //Socket.IO client
-        buttons, //Button elements
-        message, //Message element
-        score, //Score element
-        points = { //Game points
-            draw: 0,
-            win: 0,
-            lose: 0
-        };
+    let socket,
+        message,
+        board,
+        nick,
+        level
 
-    /**
-     * Disable all button
-     */
-    function disableButtons() {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].setAttribute("disabled", "disabled");
-        }
-    }
-
-    /**
-     * Enable all button
-     */
-    function enableButtons() {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].removeAttribute("disabled");
-        }
-    }
-
-    /**
-     * Set message text
-     * @param {string} text
-     */
     function setMessage(text) {
         message.innerHTML = text;
     }
 
-    /**
-     * Set score text
-     * @param {string} text
-     */
-    function displayScore(text) {
-        score.innerHTML = [
-            "<h2>" + text + "</h2>",
-            "Won: " + points.win,
-            "Lost: " + points.lose,
-            "Draw: " + points.draw
-        ].join("<br>");
-    }
-
-    /**
-     * Binde Socket.IO and button events
-     */
     function bind() {
-
-        socket.on("start", () => {
-            enableButtons();
-            setMessage("Round " + (points.win + points.lose + points.draw + 1));
-        });
-
-        socket.on("win", () => {
-            points.win++;
-            displayScore("You win!");
-        });
-
-        socket.on("lose", () => {
-            points.lose++;
-            displayScore("You lose!");
-        });
-
-        socket.on("draw", () => {
-            points.draw++;
-            displayScore("Draw!");
-        });
-
-        socket.on("end", () => {
-            disableButtons();
-            setMessage("Waiting for opponent...");
-        });
-
         socket.on("connect", () => {
-            disableButtons();
-            setMessage("Waiting for opponent...");
+          setMessage("Connected");
         });
+        socket.on("lobby", (data) => {
+          setMessage("Got lobby data");
+          console.log(data);
+          board.innerHTML='';
+          data.available.forEach((u)=>{
+            let b = document.createElement('button');
+            b.textContent = 'Play with '+u.nick+' ('+u.level+')';
+            if (u.nick==nick.value) b.disabled=true;
+            b.addEventListener('click',()=>{
+              socket.emit("play", {opponent: u.id })
+            });
+            board.appendChild(b);
+          })
+
+        });
+
 
         socket.on("disconnect", () => {
-            disableButtons();
-            setMessage("Connection lost!");
+          setMessage("Connection lost!");
+        });
+
+        socket.on("playstart", (d) => {
+          setMessage("game started; lead:"+d.lead);
+          if (d.lead)
+             socket.emit("gamemsg",{display:"I'm in charge"})
+        });
+
+        socket.on("gamemsg", (d) => {
+          setMessage("game:"+d.display);          
         });
 
         socket.on("error", () => {
-            disableButtons();
-            setMessage("Connection error!");
+          setMessage("Connection error!");
         });
 
-        for (let i = 0; i < buttons.length; i++) {
-            ((button, guess) => {
-                button.addEventListener("click", function (e) {
-                    disableButtons();
-                    socket.emit("guess", guess);
-                }, false);
-            })(buttons[i], i + 1);
-        }
+        document.getElementById("enter").addEventListener('click', ()=>{
+          socket.emit("enter", {nick: nick.value, level: level.value })
+        });
     }
 
-    /**
-     * Client module init
-     */
     function init() {
         socket = io({ upgrade: false, transports: ["websocket"] });
-        buttons = document.getElementsByTagName("button");
         message = document.getElementById("message");
-        score = document.getElementById("score");
-        disableButtons();
+        board = document.getElementById("board");
+        nick = document.getElementById("nick");
+        level = document.getElementById("lev");
         bind();
     }
 
--- public/server.js
+++ public/server.js
@@ -1,164 +1,32 @@
 "use strict";
 
-/**
- * User sessions
- * @param {Array} users
- */
 const users = [];
 
-/**
- * Find opponent for a user
- * @param {User} user
- */
-function findOpponent(user) {
-	for (let i = 0; i < users.length; i++) {
-		if (
-			user !== users[i] &&
-			users[i].opponent === null
-		) {
-			new Game(user, users[i]).start();
-		}
-	}
-}
+function pubUsers() {
+  const lp = users.filter(u => u.status == 1)
+    .map(u => ({
+			id: u.id,
+      nick: u.nick,
+      level: u.level,
+    }));
+  const pp = users.filter(u => u.status > 1)
+    .map(u => ({
+      id: u.id,
+      nick: u.nick,
+      level: u.level,
+    }));
 
-/**
- * Remove user session
- * @param {User} user
- */
-function removeUser(user) {
-	users.splice(users.indexOf(user), 1);
-}
+  users.filter(u => u.status == 1).forEach(u => u.emit("lobby", {
+    available: lp,
+    playing: pp,
+  }));
+};
 
-/**
- * Game class
- */
-class Game {
-
-	/**
-	 * @param {User} user1
-	 * @param {User} user2
-	 */
-	constructor(user1, user2) {
-		this.user1 = user1;
-		this.user2 = user2;
-	}
-
-	/**
-	 * Start new game
-	 */
-	start() {
-		this.user1.start(this, this.user2);
-		this.user2.start(this, this.user1);
-	}
-
-	/**
-	 * Is game ended
-	 * @return {boolean}
-	 */
-	ended() {
-		return this.user1.guess !== GUESS_NO && this.user2.guess !== GUESS_NO;
-	}
-
-	/**
-	 * Final score
-	 */
-	score() {
-		if (
-			this.user1.guess === GUESS_ROCK && this.user2.guess === GUESS_SCISSORS ||
-			this.user1.guess === GUESS_PAPER && this.user2.guess === GUESS_ROCK ||
-			this.user1.guess === GUESS_SCISSORS && this.user2.guess === GUESS_PAPER
-		) {
-			this.user1.win();
-			this.user2.lose();
-		} else if (
-			this.user2.guess === GUESS_ROCK && this.user1.guess === GUESS_SCISSORS ||
-			this.user2.guess === GUESS_PAPER && this.user1.guess === GUESS_ROCK ||
-			this.user2.guess === GUESS_SCISSORS && this.user1.guess === GUESS_PAPER
-		) {
-			this.user2.win();
-			this.user1.lose();
-		} else {
-			this.user1.draw();
-			this.user2.draw();
-		}
-	}
-
-}
-
-/**
- * User session class
- */
-class User {
-
-	/**
-	 * @param {Socket} socket
-	 */
-	constructor(socket) {
-		this.socket = socket;
-		this.game = null;
-		this.opponent = null;
-		this.guess = GUESS_NO;
-	}
-
-	/**
-	 * Set guess value
-	 * @param {number} guess
-	 */
-	setGuess(guess) {
-		if (
-			!this.opponent ||
-			guess <= GUESS_NO ||
-			guess > GUESS_SCISSORS
-		) {
-			return false;
-		}
-		this.guess = guess;
-		return true;
-	}
-
-	/**
-	 * Start new game
-	 * @param {Game} game
-	 * @param {User} opponent
-	 */
-	start(game, opponent) {
-		this.game = game;
-		this.opponent = opponent;
-		this.guess = GUESS_NO;
-		this.socket.emit("start");
-	}
-
-	/**
-	 * Terminate game
-	 */
-	end() {
-		this.game = null;
-		this.opponent = null;
-		this.guess = GUESS_NO;
-		this.socket.emit("end");
-	}
-
-	/**
-	 * Trigger win event
-	 */
-	win() {
-		this.socket.emit("win", this.opponent.guess);
-	}
-
-	/**
-	 * Trigger lose event
-	 */
-	lose() {
-		this.socket.emit("lose", this.opponent.guess);
-	}
-
-	/**
-	 * Trigger draw event
-	 */
-	draw() {
-		this.socket.emit("draw", this.opponent.guess);
-	}
-
+function removeUser(user)
+{
+  let i=users.findIndex(u=>u.id==user.id);
+	if (i>=0)
+    users.splice(i, 1);
 }
 
 /**
@@ -167,38 +35,60 @@ class User {
  */
 module.exports = {
 
-	io: (socket) => {
-		const user = new User(socket);
-		users.push(user);
-		findOpponent(user);
+  io: (socket) => {
+    const user = {
+      id: +(new Date()),
+      nick: null,
+      level: null,
+      status: 0,
+      emit: (k, d) => socket.emit(k, d),
+    };
+    console.log("Connected: " + socket.id);
 
-		socket.on("disconnect", () => {
-			console.log("Disconnected: " + socket.id);
-			removeUser(user);
-			if (user.opponent) {
-				user.opponent.end();
-				findOpponent(user.opponent);
-			}
+    socket.on("disconnect", () => {
+      console.log("Disconnected: " + socket.id);
+      removeUser(user);
+			pubUsers();
+    });
+
+    socket.on("enter", (ui) => {
+			removeUser(user)
+      user.nick = ui.nick;
+      user.level = ui.level;
+      user.status = 1;
+      users.push(user);
+      pubUsers();
+    });
+
+		socket.on("leave", (ui) => {
+			removeUser(user)
+			pubUsers();
 		});
 
-		socket.on("guess", (guess) => {
-			console.log("Guess: " + socket.id);
-			if (user.setGuess(guess) && user.game.ended()) {
-				user.game.score();
-				user.game.start();
-				storage.get('games', 0).then(games => {
-					storage.set('games', games + 1);
-				});
-			}
+		socket.on("play", (d) => {
+       let lead=users.find(u=>u.id==d.opponent);
+			 if (lead&&(lead.status==1)) { //we match
+				 lead.status=2;
+				 lead.match=user;
+				 user.status=3;
+				 user.match=lead;
+				 lead.emit("playstart",{lead: true});
+				 user.emit("playstart",{lead: false});
+				 pubUsers();
+			 }
 		});
 
-		console.log("Connected: " + socket.id);
-	},
-
-	stat: (req, res) => {
-		storage.get('games', 0).then(games => {
-			res.send(`<h1>Games played: ${games}</h1>`);
+		socket.on("gamemsg", (d) => {
+    	 if (user.match) { //we match
+				 user.match.emit("gamemsg",d);
+			 }
 		});
-	}
+
+		socket.on("playend", (d) => {
+    	 user.match=null;
+			 user.status=1;
+			 pubUsers();
+		});
+  },
 
 };
